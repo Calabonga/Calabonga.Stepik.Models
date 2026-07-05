daae3571a69c753d67ea5937e5a8b005db33d92d
Sergei Calabonga <calabonga@outlook.com>
2026-07-05 14:23:10 +0700

    First release

---
+++ src/Calabonga.Stepik.Models/Meta.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Calabonga.Stepik.Models
+{
+    public class Meta
+    {
+        [JsonPropertyName("meta")]
+        public int Page { get; set; }
+        
+        [JsonPropertyName("has_next")]
+        public bool HasNext { get; set; }
+        
+        [JsonPropertyName("has_previous")]
+        public bool HasPrevious { get; set; }
+    }
+}
---
+++ src/Calabonga.Stepik.Models/StepikResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Calabonga.Stepik.Models
+{
+    public class StepikResponse
+    {
+        [JsonPropertyName("meta")]
+        public Meta? Meta { get; set; }
+
+        [JsonPropertyName("courses")]
+        public Course[]? Courses { get; set; }
+    }
+}
